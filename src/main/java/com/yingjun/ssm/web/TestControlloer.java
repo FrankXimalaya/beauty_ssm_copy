11c2aa390786a20fb9eb55e88c506bd3c6b25b01
VULCAN <13065722318@163.com>
2017-10-02 15:20:40 +0800

    第一次用git提交

---
+++ src/main/java/com/yingjun/ssm/web/TestControlloer.java
@@ -0,0 +1,17 @@
+package com.yingjun.ssm.web;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class TestControlloer {
+
+    public static void main(String[] args) {
+
+        System.out.println("哈哈哈");
+        System.out.println();
+
+
+    }
+
+}
+
